cabc07c6420971413d720102baf04948a74b99ee
ferre <3178251769@qq.com>
2019-12-25 17:47:14 +0800

    add mobile-check

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -14,12 +14,24 @@ class ComposerStaticInit1ce179c9666a056e28414c948d3b67a9
                 0 => __DIR__ . '/..' . '/erusev/parsedown',
             ),
         ),
+        'D' => 
+        array (
+            'Detection' => 
+            array (
+                0 => __DIR__ . '/..' . '/mobiledetect/mobiledetectlib/namespaced',
+            ),
+        ),
+    );
+
+    public static $classMap = array (
+        'Mobile_Detect' => __DIR__ . '/..' . '/mobiledetect/mobiledetectlib/Mobile_Detect.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixesPsr0 = ComposerStaticInit1ce179c9666a056e28414c948d3b67a9::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit1ce179c9666a056e28414c948d3b67a9::$classMap;
 
         }, null, ClassLoader::class);
     }
